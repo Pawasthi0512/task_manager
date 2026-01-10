26db5714fcb3f32ad4baf1e499ae0d4a742b2cd0
awasthiz0512 <puneeth.awasthi@springhealth.com>
2026-01-10 19:46:09 +0000

    Add SimpleCov for code coverage analysis and update .gitignore to exclude coverage directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,17 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require_relative 'support/request_spec_helper'
+
+require 'simplecov'
+
+# Start SimpleCov for code coverage analysis
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # Ignore the tests themselves
+end
+
+# ... existing RSpec configuration ...
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
